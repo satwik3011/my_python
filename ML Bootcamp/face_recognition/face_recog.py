a4501cdcffb560b3944d22e90a460316ae6181c1
satwik3011 <satwikdudeja@gmail.com>
2020-09-24 22:20:02 +0530

    Added projects

--- ML Bootcamp/face_recognition/face_recog.py
+++ ML Bootcamp/face_recognition/face_recog.py
@@ -22,18 +22,20 @@ while True:
         faces = detector(frame)
 
         for face in faces:
-            x = face.left()
-            y = face.top()
-            w = face.right() - x
-            h = face.bottom() - y
 
-            cut = frame[y:y + h, x:x + w]
-            fix = cv2.resize(cut, (100, 100))
-            gray = cv2.cvtColor(fix, cv2.COLOR_BGR2GRAY)
-            out = model.predict([gray.flatten()])
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-            cv2.putText(frame, str(out[0]), (x, y - 10), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 2)
-            cv2.imshow("My Face", gray)
+            if face:
+                x = face.left()
+                y = face.top()
+                w = face.right() - x
+                h = face.bottom() - y
+
+                cut = frame[y:y + h, x:x + w]
+                fix = cv2.resize(cut, (100, 100))
+                gray = cv2.cvtColor(fix, cv2.COLOR_BGR2GRAY)
+                out = model.predict([gray.flatten()])
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+                cv2.putText(frame, str(out[0]), (x, y - 10), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 2)
+                cv2.imshow("My Face", gray)
 
         cv2.imshow("My Screen", frame)
 
